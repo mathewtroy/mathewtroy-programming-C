86cdcedabe342b7c3a1bb87b478f77913e806584
mathewtroy <75392252+mathewtroy@users.noreply.github.com>
2021-09-22 18:36:31 +0300

    Update HW01-calculator-from-10-to-16.c

--- HW01-calculator-from-10-to-16.c
+++ HW01-calculator-from-10-to-16.c
@@ -1,10 +1,11 @@
-//HW001
+// HW001
 #include <stdio.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   long int decNum1,first,decNum2,second,decFirst=0,decSec=0,lenHexaFirst,lenHexaSec,lenDeciFirst,lenDeciSec;
-  int i=1,tmp, soucet, rozdil, soucin, podil;
+  int tmp, soucet, rozdil, soucin, podil;
   float prumer;
   char strFirst, strSec;
         
@@ -14,7 +15,7 @@ int main() {
             printf("Vstup je mimo interval!");
             return 1;
     }
-    if (  decNum2<-10000 || decNum2>10000 ){
+    if ( decNum2<-10000 || decNum2>10000 ){
             printf("Vstup je mimo interval!");
             return 1;
     }
@@ -22,14 +23,14 @@ int main() {
   first = decNum1;
   second = decNum2;
 
-        for(lenDeciFirst=first; lenDeciFirst>0; lenDeciFirst=lenDeciFirst/16) {
+        for(lenDeciFirst=abs(first); lenDeciFirst>0; lenDeciFirst=lenDeciFirst/16) {
           tmp = lenDeciFirst % 16;
           if( tmp < 10)
           tmp = tmp + 48; else
           tmp = tmp + 55;
           decFirst=decFirst*100+tmp;
         }
-              for(lenDeciSec=second; lenDeciSec>0; lenDeciSec=lenDeciSec/16) {
+              for(lenDeciSec=abs(second); lenDeciSec>0; lenDeciSec=lenDeciSec/16) {
                 tmp = lenDeciSec % 16;
                 if( tmp < 10)
                 tmp = tmp + 48; else
@@ -40,12 +41,25 @@ int main() {
     printf("Decitkova soustava: %ld  %ld", decNum1, decNum2);
     printf("\nSestnactkova soustava: ");
 
+    if(decNum1==0)
+            printf(" 0");    
+            else if(decNum1<0)
+            printf(" -");
+            else (decNum1>0);
+            printf(" ");
+
         for(lenHexaFirst=decFirst;lenHexaFirst>0;lenHexaFirst=lenHexaFirst/100) {
                strFirst=lenHexaFirst % 100;
                printf("%c",strFirst);
         }
 
-    printf("  ");
+    if(decNum2==0)
+            printf(" 0");    
+            else if(decNum2<0)
+            printf(" -");
+            else (decNum2>0);
+            printf(" ");
+
         for(lenHexaSec=decSec;lenHexaSec>0;lenHexaSec=lenHexaSec/100) {
                strSec=lenHexaSec % 100;
                printf("%c",strSec);
@@ -59,11 +73,16 @@ int main() {
     printf("\nSoucet: %ld + %ld = %d ", decNum1, decNum2, soucet );
     printf("\nRozdil: %ld - %ld = %d ", decNum1, decNum2, rozdil );
     printf("\nSoucin: %ld * %ld = %d ", decNum1, decNum2, soucin );
-            if(decNum2==0)
-            printf("\nNedefinovany vysledek!");
-            else
-            podil = decNum1/decNum2;
-            printf("\nPodil: %ld / %ld = %d ", decNum1, decNum2, podil );
+            
+            if(decNum2==0) {
+              printf("\nNedefinovany vysledek!");
+            }
+
+            else { 
+              podil = decNum1/decNum2;
+              printf("\nPodil: %ld / %ld = %d ", decNum1, decNum2, podil );
+            }
+
     printf("\nPrumer: = %.1f ", prumer );
 
 return 0;
